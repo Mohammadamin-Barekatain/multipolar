dde653e6516469567a804dc0984b77f7da2b9cb5
mohammadamin-barekatain <m.barekatain@tum.de>
2019-04-19 16:20:12 +0900

    bash script for parameter search

---
+++ searchParamEnv.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+for main_engine_power in 1 7 13 26 39
+do
+    for scale in 10 20 30 40 50
+    do
+        for seed in 1000 2000 3000
+        do
+            exp_name="$main_engine_power-$scale"
+            cmd="python train.py --env LunarLanderContinuous-v2 --algo ppo2 --seed $seed --exp-name $exp_name --MAIN_ENGINE_POWER $main_engine_power --SCALE $scale"
+            `$cmd`
+        done
+    done
+done
