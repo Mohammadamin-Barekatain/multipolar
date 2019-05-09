3fdc4b9ee71c30e28992b4dbd01d01343ddf13fb
mohammadamin-barekatain <m.barekatain@tum.de>
2019-05-09 11:49:32 +0900

    better param search

--- searchParamEnv.sh
+++ searchParamEnv.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+algo="ppo2"
+video_interval=10
+
 for main_engine_power in 10 13 18 23 28 33 38
 do
     for scale in 25 30 35 40 45
@@ -7,9 +10,9 @@ do
         for seed in 1000 2000 3000
         do
             exp_name="$main_engine_power-$scale"
-            cmd="python train.py --env LunarLanderContinuous-v2 --algo ppo2 --seed $seed --exp-name $exp_name \
-            --play 1000 --save_video_interval 10 --save_video_length 500 \
-             --MAIN_ENGINE_POWER $main_engine_power --SCALE $scale"
+            cmd="python train.py --env LunarLanderContinuous-v2 --algo $algo \
+            --play 1000 --save_video_interval $video_interval --save_video_length 300 --seed $seed --exp-name $exp_name \
+            --MAIN_ENGINE_POWER $main_engine_power --SCALE $scale"
             echo $cmd
         done
     done
