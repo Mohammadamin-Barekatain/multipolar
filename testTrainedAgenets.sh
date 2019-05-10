b70cc9ab67b5c30c4a7e1252b591eb6766833a1d
mohammadamin-barekatain <m.barekatain@tum.de>
2019-05-10 16:33:18 +0900

    bug fixed in testing sh

--- testTrainedAgenets.sh
+++ testTrainedAgenets.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+#algo="ppo2"
+#n_envs=40
+
+algo="sac"
+n_envs=1
+
 for mep in 10 13 18 23 28 33 38
 do
     for scl in 25 30 35 40 45
@@ -8,14 +14,13 @@ do
         do
             for scale in 25 30 35 40 45
             do
-                for ind in 1 2 3
-                do
-                    exp_name="$main_engine_power-$scale_$ind"
-                    train_name="$mep-$scl"
-                    cmd="python test.py --trained-agent logs/ppo2/LunarLanderContinuous-v2_""$train_name""_1/LunarLanderContinuous-v2.pkl \
-                     --exp-name $exp_name --MAIN_ENGINE_POWER $main_engine_power --SCALE $scale --n-envs 40"
-                    echo $cmd
-                done
+
+                exp_name="$main_engine_power-$scale"
+                train_name="$mep-$scl"
+                cmd="python test.py --trained-agent logs/$algo/LunarLanderContinuous-v2_""$train_name""_1/LunarLanderContinuous-v2.pkl \
+                 --exp-name $exp_name --MAIN_ENGINE_POWER $main_engine_power --SCALE $scale --n-envs $n_envs"
+                echo $cmd
+
             done
         done
     done
