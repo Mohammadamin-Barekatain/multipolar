bd18854ff541cda6d2f095591de7ac379437a03f
mohammadamin-barekatain <m.barekatain@tum.de>
2019-07-08 17:03:12 +0900

    bug fix in create test env

--- utils/utils.py
+++ utils/utils.py
@@ -136,6 +136,9 @@ def create_test_env(env_id, n_envs=1, stats_path=None, seed=0, log_dir=None, sho
         if hyperparams['normalize']:
             print("Loading running average")
             print("with params: {}".format(hyperparams['normalize_kwargs']))
+            if 'norm_reward' in hyperparams['normalize_kwargs']:
+                del hyperparams['normalize_kwargs']['norm_reward']
+
             env = VecNormalize(env, training=False, **hyperparams['normalize_kwargs'], norm_reward=False)
             env.load_running_average(stats_path)
 
