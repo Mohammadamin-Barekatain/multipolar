ac7821e64a3eb240d532ef651d044e2bc55631f4
mohammadamin-barekatain <m.barekatain@tum.de>
2019-08-08 16:07:47 +0900

    ant training

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -13,7 +13,7 @@ import gym
 import numpy as np
 from gym.envs.box2d import LunarLander, BipedalWalker, CarRacing, BipedalWalkerHardcore
 from gym.envs.classic_control import AcrobotEnv
-from roboschool import RoboschoolHopper
+from roboschool import RoboschoolHopper, RoboschoolAnt
 
 
 class ModifyBox2DEnvParams(gym.Wrapper):
@@ -82,6 +82,11 @@ def _get_string_from_tuple(s):
     s = str(s)
     s = s[1:-1]
     return s.replace(',', '')
+
+
+def _get_tuple_from_string(s):
+    s = s.replace(' ', ',')
+    return literal_eval(s)
 
 
 def _get_end(body):
@@ -189,6 +194,77 @@ class ModifyHopperEnvParams(gym.Wrapper):
         tree.write(save_file)
 
 
+class ModifyAntEnvParams(gym.Wrapper):
+
+    def __init__(self, env, save_file, verbose, **params):
+        """
+        Modify the parameters of the given Roboschool Hopper environment with params.
+
+        env: (Gym Environment) the environment to wrap
+        params: the parameters to change in env
+        """
+        gym.Wrapper.__init__(self, env)
+        self.params = params
+
+        # find the path to the source code of the environment
+        path = inspect.getfile(env.env.__class__)[:-3]
+        idx = path.rfind('/')
+        path = path[:idx]
+        path = os.path.join(path, 'mujoco_assets/ant.xml')
+
+        # load xml file specifying hopper dynamics and Kinematics
+        tree = ET.parse(path)
+        root = tree.getroot()
+        body_parts = {body.attrib['name']: body for body in root.iter('body')}
+
+        # change the parameters of the environment
+        for key, val in params.items():
+            val = float(val)
+            if 'length' == key:
+                for name, body in body_parts.items():
+                    if name != 'torso':
+                        geom = body.find('geom')
+                        fromto = _get_tuple_from_string(geom.attrib['fromto'])
+                        fromto = tuple([val*x for x in fromto])
+                        fromto = _get_string_from_tuple(fromto)
+                        geom.set('fromto', fromto)
+                        if verbose > 0:
+                            print("length of {} changed to {}".format(name, fromto))
+
+                        pos = _get_tuple_from_string(body.attrib['pos'])
+                        pos = tuple([val * x for x in pos])
+                        pos = _get_string_from_tuple(pos)
+                        body.set('pos', pos)
+
+            elif key == 'size':
+                for name, body in body_parts.items():
+                    geom = body.find('geom')
+                    size = float(geom.attrib['size'])
+                    print(name, body, size)
+                    size = size * val
+                    geom.set('size', str(size))
+                    if verbose > 0:
+                        print("size of {} changed to {}".format(name, size))
+
+            elif key == 'damping' or key == 'armature':
+                joint = root.find('default').find('joint')
+                joint.set(key, str(val))
+                if verbose > 0:
+                    print("{} changed to {}".format(key, val))
+
+            elif key == 'friction':
+                geom = root.find('default').find('geom')
+                val = "{} {} {}".format(val, val/15, val/15)
+                geom.set(key, val)
+                if verbose > 0:
+                    print("{} changed to {}".format(key, val))
+
+            else:
+                raise ValueError('{} is either not a parameter in the env {} or not supported'.format(key, env))
+
+        tree.write(save_file)
+
+
 def modify_env_params(env, params_path=None, verbose=1, **params):
 
     if verbose > 0:
@@ -205,6 +281,18 @@ def modify_env_params(env, params_path=None, verbose=1, **params):
         assert params_path is not None, "params_path must be provided for modifying Hopper"
         save_file = os.path.join(params_path, "Hopper.xml")
         env = ModifyHopperEnvParams(env=env, save_file=save_file, verbose=verbose, **params)
+        if hasattr(env.env, 'model_xml'):
+            env.env.model_xml = '/' + save_file
+        elif hasattr(env.env.env, 'model_xml'):
+            env.env.env.model_xml = '/' + save_file
+        else:
+            raise ValueError("cannot change the env, please check the source code")
+        return env
+
+    elif isinstance(env.env, RoboschoolAnt):
+        assert params_path is not None, "params_path must be provided for modifying Ant"
+        save_file = os.path.join(params_path, "ant.xml")
+        env = ModifyAntEnvParams(env=env, save_file=save_file, verbose=verbose, **params)
         if hasattr(env.env, 'model_xml'):
             env.env.model_xml = '/' + save_file
         elif hasattr(env.env.env, 'model_xml'):
